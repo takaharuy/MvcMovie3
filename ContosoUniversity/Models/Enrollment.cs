2df270356691527a42b0bcd30b9226c7c632605b
Takaharu Yuji <takaharu_y@hotmail.co.jp>
2020-11-03 06:28:38 +0900

    Create the data model

---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, f
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+        public Grade? Grade { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
