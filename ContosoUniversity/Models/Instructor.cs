524955912ae9329997c3c901394224f86f15c2d1
湯治高治 <takaharu-y@ujit67.onmicrosoft.com>
2020-11-09 17:14:39 +0900

    Create Instructor Entity

--- ContosoUniversity/Models/Instructor.cs
+++ ContosoUniversity/Models/Instructor.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ContosoUniversity.Migrations;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +10,30 @@ namespace ContosoUniversity.Models
 {
     public class Instructor
     {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column("FirstName")]
+        [StringLength(50)]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return LastName + ", " + FirstMidName; }
+        }
+
+        public ICollection<CourseAssignment> CourseAssignmens { get; set; }
+        public OfficeAssignment OfficeAssignment { get; set; }
     }
 }
