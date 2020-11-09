4dc96dd14202caf470ac94fbc39d9c1c5b019d0a
湯治高治 <takaharu-y@ujit67.onmicrosoft.com>
2020-11-09 15:29:10 +0900

    The Column attribute

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -9,8 +9,15 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int Id { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [StringLength(50)]
         public string LastName { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [StringLength(50)]
         public string FirstMidName { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
