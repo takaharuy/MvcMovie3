905e283d307deef666f5fc7f23db32628cf0b92f
湯治高治 <takaharu-y@ujit67.onmicrosoft.com>
2020-11-09 14:59:48 +0900

    The DataType attribute

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace ContosoUniversity.Models
         public int Id { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
