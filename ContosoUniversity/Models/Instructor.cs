052e7fd586de84efddf3b407661ed552e12b6375
湯治高治 <takaharu-y@ujit67.onmicrosoft.com>
2020-11-17 16:28:08 +0900

    Return update Instructor

--- ContosoUniversity/Models/Instructor.cs
+++ ContosoUniversity/Models/Instructor.cs
@@ -8,12 +8,33 @@ using System.Threading.Tasks;
 
 namespace ContosoUniversity.Models
 {
-    public class Instructor : Person
+    public class Instructor
     {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name ="Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column("FirstName")]
+        [StringLength(50)]
+        public string FirstMidName { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Hire Date")]
         public DateTime HireDate { get; set; }
+
+        [Display(Name ="Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
 
         public ICollection<CourseAssignment> CourseAssignmens { get; set; }
         public OfficeAssignment OfficeAssignment { get; set; }
